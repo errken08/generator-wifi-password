98c6d2bf4d1d1d3d6c83c1347cf649befed283fa
errken08 <kenesbekovyernaz006@gmail.com>
2023-05-04 23:41:26 +0600

    add main.py

---
+++ generator_password/gPass.py
@@ -0,0 +1,24 @@
+import random
+import wifi_qrcode_generator.generator
+
+
+names = ['Abylai', 'Ainur', 'Yelnaz', 'Yernaz', 'Aizere', 'Edil']
+
+def main():
+	SSID = random.choice(names)
+	PASS = random.randrange(10000000000)
+	text = f"""
+Wi-Fi
+SSID: {SSID}
+Password: {PASS}
+	"""
+	print(text)
+
+	with open('Wifi.txt', 'a') as file:
+		file.write(text)
+
+	qr_code = wifi_qrcode_generator.generator.wifi_qrcode(
+    ssid=SSID, hidden=False, authentication_type='WPA', password=PASS
+	)
+	qr_code.print_ascii()
+	qr_code.make_image().save('wifi_qr.png')
---
+++ main.py
@@ -0,0 +1,5 @@
+from generator_password import gPass
+
+
+if __name__ == '__main__':
+	gPass.main()
